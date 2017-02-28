a157bedb16ecf26b61af1165506dc7f8a0c998b0
ITLAB <ITLAB@IT209>
2017-02-28 15:45:55 +0530

    second commit

--- src/com/example/multithreading/MainActivity.java
+++ src/com/example/multithreading/MainActivity.java
@@ -1,35 +1,41 @@
-package com.example.multithreading;
+package com.nkm.thread; 
+import android.app.Activity; 
+import android.os.Bundle; 
+import android.os.Handler; 
+import android.widget.Button; 
+import android.widget.TextView; 
+public class MultiThreadingActivity extends Activity { Handler hand = new Handler(); 
+Button clickme; 
+TextView timer;
+@Override 
+public void onCreate(Bundle_savedInstance) 
+{ 
+	super.onCreate(savedInstanceState); 
+	setContentView(R.layout.main); 
+	timer = (TextView) findViewById(R.id_timer); 
+	clickme = (Button) findViewById(R.id_clickme);
+	hand.postDelayed(run, 1000);
+	} 
+Runnable run = new Runnable() 
+{
+	@Override 
+	public void run()
+	{ updateTime();
+	}
+	};
+	public void updateTime() 
+	{ 
+		timer.setText("" + (Integer.parseInt(timer.getText().toString()) - 1));
+		
+	
+		if (Integer.parseInt(timer.getText().toString()) == 0) 
+		{ 
+			clickme.setVisibility(0);
+			} 
+		else { hand.postDelayed(run, 1000);
+ } 
+		} 
+	}
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}
